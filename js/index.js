778cf64ea9a0ae32f9bf76bc5669771810adc055
Mckenney <oluwarinolasam@gmail.com>
2020-08-06 09:25:56 +0100

    two players also initiated

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import { event } from '../modules/manipFuncs.js';
 import playersSelected from '../modules/playerSelect.js';
 import { gameStart } from '../modules/gameStartInit.js';
 import onePlayerGamePlay from '../modules/onePlayerGamePlay.js';
+import twoPlayersGamePlay from '../modules/twoPlayersGamePlay.js';
 
 const { onePlayerBtn, twoPlayersBtn, startGameBtn } = DOMElems;
 
@@ -10,7 +11,11 @@ event(onePlayerBtn, 'click', playersSelected);
 event(twoPlayersBtn, 'click', playersSelected);
 
 event(startGameBtn, 'click', (ev) => {
-    if (ev.target.parentElement.children[0].textContent === 'One Player') {
-        if (gameStart()) onePlayerGamePlay();
+    if (gameStart()) {
+        if (ev.target.parentElement.children[0].textContent === 'One Player') {
+            onePlayerGamePlay();
+        } else {
+            twoPlayersGamePlay();
+        }
     }
 });
---
+++ modules/twoPlayersGamePlay.js
@@ -0,0 +1,14 @@
+import {
+    player1,
+    opponent,
+    currentPlayer,
+} from './gameStartInit.js';
+const twoPlayersGamePlay = () => {
+    console.log({
+        player1,
+        opponent,
+        currentPlayer,
+    });
+};
+
+export default twoPlayersGamePlay;
